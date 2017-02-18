17c914ba1ddd49a8b15e7c3fdaaddbdbabf3d739
Scott Tromley <scott@tromley.ca>
2017-02-18 00:47:46 -0500

    Add movement with arrow keys

---
+++ Jam IT/Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float playerVelocity = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(playerVelocity * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, playerVelocity * Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
