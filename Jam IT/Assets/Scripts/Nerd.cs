81b3d2acf4b8d9f73754c27853c936a8542bb2af
Ricardo Bandala <ricardobandala@gmail.com>
2017-02-18 06:40:03 -0500

    script to follow

---
+++ Jam IT/Assets/Scripts/Nerd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nerd : MonoBehaviour {
+
+	 public Transform goal;
+       
+       void Start () {
+          UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+          agent.destination = goal.position; 
+       }
+       void Update () {
+          UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+          agent.destination = goal.position; 
+       }
+}
