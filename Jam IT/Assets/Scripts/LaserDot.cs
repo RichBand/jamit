ab70b1abc01813b5ab1295f1e0a31db584a940c2
Scott Tromley <scott@tromley.ca>
2017-02-18 04:50:07 -0500

    Fix laser pointer distance

--- Jam IT/Assets/Scripts/LaserDot.cs
+++ Jam IT/Assets/Scripts/LaserDot.cs
@@ -8,12 +8,15 @@ public class LaserDot : MonoBehaviour {
 	public bool laserActivated = false;
 	public int batteryLevel;
 	public Vector3 laserSize;
+	public GameObject playerObject;
+	public Vector3 lastPosition = Vector3.zero;
 	// Use this for initialization
 	void Start () {
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
+		//Cursor.lockState = CursorLockMode.Locked;
+		//Cursor.visible = false;
 		batteryLevel = 100;
 		laserSize = this.transform.localScale;
+		playerObject = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
@@ -29,14 +32,36 @@ public class LaserDot : MonoBehaviour {
 			this.transform.localScale = laserSize;
 		}
 		//Debug.Log("Battery level: " + batteryLevel);
-		var playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
-		var h = horizontalSpeed * Input.GetAxis ("Mouse X");
- 		var v = verticalSpeed * Input.GetAxis ("Mouse Y");
-		Vector3 diff = this.transform.position - playerPosition;
-		float distance = diff.magnitude;
-		if (distance > 50) {
-			this.transform.position = playerPosition + (diff / distance) * 50;			
+		var playerPosition = playerObject.transform.position;
+		//var h = horizontalSpeed * Input.GetAxis ("Mouse X");
+ 		//var v = verticalSpeed * Input.GetAxis ("Mouse Y");
+		//Vector3 diff = this.transform.position - playerPosition;
+		
+		RaycastHit mouseCollision;
+		Vector3 worldPoint = Vector3.zero;
+		bool didItHit = false;
+		Ray test = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if ( Physics.Raycast(test, out mouseCollision )) {
+			{
+				didItHit = true;
+				worldPoint = mouseCollision.point;
+			}
+
 		}
-		transform.Translate (h , 0, v);
+
+		
+		//var test = new Vector3(this.transform.position.x + h, this.transform.position.y, this.transform.position.z + v);
+		lastPosition = this.transform.position;
+		float distance = Vector3.Distance(worldPoint, playerPosition);//diff.magnitude;
+		//float distance = Vector3.Distance(this.transform.position, playerPosition);//diff.magnitude;
+		Debug.Log(distance);
+		
+		if (distance <= 50 && didItHit) {
+			this.transform.position = worldPoint;
+		} else {
+			//this.transform.position = lastPosition;
+			this.transform.position = playerPosition + ((worldPoint - playerPosition).normalized * 50);
+		}
+	
 	}
 }
