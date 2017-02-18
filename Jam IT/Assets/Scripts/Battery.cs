9c643c950581f3129e2119d5989dcf6a06a3c322
Ricardo Bandala <ricardobandala@gmail.com>
2017-02-18 18:00:34 -0500

    merge problem on battery and workspace

---
+++ Jam IT/Assets/Scripts/Battery.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour {
+	public GameObject center;
+	public Vector3 size;
+	public float hoverHeight = 1f;
+	public GameObject BatteryPrefab;
+
+	// Use this for initialization
+	void Start () {	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void HiddenPosition(){
+		gameObject.SetActive(false);
+	}
+	void OnTriggerEnter(Collider other){
+
+		if( other.gameObject.tag == "PlayerHitbox"  ){
+			HiddenPosition();
+			Invoke( "RandomPosition" , 3f );		
+		}
+	}
+	public void RandomPosition(){
+		gameObject.SetActive(true);
+		if (center != null) {
+			Debug.Log ("random");
+			float x = center.transform.position.x + (Random.Range(-size.x / 2, size.x / 2 ));
+			float z = center.transform.position.z + (Random.Range(-size.z / 2, size.z / 2 ));
+			transform.position = new Vector3(x, center.transform.position.y, z);
+			Debug.Log (x + ", " + z + " ->" + transform.position);
+		} else {
+			Debug.Log ("bad stuff");
+		}
+	}
+
+}
---
+++ Jam IT/Assets/Scripts/Workspace.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Workspace : MonoBehaviour {
+	private GameObject battery;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+	battery = other.gameObject;
+		if( other.gameObject.tag == "BatteryHitbox"  ){	
+			battery.GetComponent<Battery>().HiddenPosition();
+			battery.GetComponent<Battery>().RandomPosition();
+			Debug.Log("battery and worspace collide");
+		}
+		Debug.Log(other.gameObject.tag);
+		if( other.gameObject.tag == "PlayerHitbox" || other.gameObject.tag == "Player"  ){	
+			//transform.position = new Vector3(6.76, -2.28, 1.37);
+			Debug.Log("Player and workspace collide");
+		}
+	}
+}
