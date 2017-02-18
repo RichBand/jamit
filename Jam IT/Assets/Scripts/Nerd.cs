fab48c20d3789c01d30bebfc0fc71f07dcbeb1de
Scott Tromley <scott@tromley.ca>
2017-02-18 09:54:46 -0500

    Add laserbeam, fix enemy pathing

---
+++ Jam IT/Assets/Scripts/Nerd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nerd : MonoBehaviour {
+
+	 public Transform goal;
+     UnityEngine.AI.NavMeshAgent agent;
+       void Start () {
+          agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+          agent.destination = goal.position; 
+       }
+       void Update () {
+           transform.eulerAngles=new Vector3(0,0,0);
+           agent.SetDestination(goal.position);
+       }
+}
