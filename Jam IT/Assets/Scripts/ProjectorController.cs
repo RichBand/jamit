0c37761c67787ecf374615fe792eb995ef90a798
Scott Tromley <scott@tromley.ca>
2017-02-18 21:36:28 -0500

    Add projector laser interaction with charging etc. Still a bug to do with projectorLight object not being defined somehow that needs fixing.

---
+++ Jam IT/Assets/Scripts/ProjectorController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ProjectorController : MonoBehaviour {
+public float projectorCharge;
+public float maxBrightness;
+public Light projectorLight;
+
+
+	// Use this for initialization
+	void Start () {
+		Transform projector = transform.FindChild( "Projector" );
+		projectorLight = projector.GetComponent<Light>();
+		
+		maxBrightness = projectorLight.intensity;
+		projectorLight.intensity = 0f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(projectorCharge < 1.0f){
+			projectorLight.intensity = projectorCharge * maxBrightness;
+		}
+		if(projectorCharge >= 1.0f) {
+			Debug.Log("WE ARE FULLY CHARGED!");
+		}
+		
+	}
+}
---
+++ Jam IT/Assets/Scripts/ProjectorScreenController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectorScreenController : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject laserDot;
+	void Start () {
+		laserDot = GameObject.FindGameObjectWithTag("LaserDot");
+	}
+	void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "LaserDot" &&  laserDot.GetComponent<LaserDotController>().laserActivated == true && this.GetComponentInParent<ProjectorController>().projectorCharge < 1.0f) {
+            this.GetComponentInParent<ProjectorController>().projectorCharge+=.11f; 
+        }
+       }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
