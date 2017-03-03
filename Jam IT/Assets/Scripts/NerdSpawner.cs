fc4fef045e3071f4a2b7beba3956e41d35cba252
Matthew Davey <matthew.davey@dotbunny.com>
2017-03-03 13:33:20 -0500

    Add NerdSpawner Example

---
+++ Jam IT/Assets/Scripts/NerdSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NerdSpawner : MonoBehaviour {
+
+	/// <summary>
+	/// This is a reference to the prefab in the project which will be spawned.
+	/// </summary>
+    public GameObject NerdPrefab;
+    // Use this for initialization
+    public Transform SpawnLocation;
+
+    public void Spawn()
+	{
+		// Spawn nerd, at this transform position (so you would want to replace that with somethign, at a zero'd rotation (identity))
+        GameObject newObject = (GameObject)Object.Instantiate(NerdPrefab, SpawnLocation.position, Quaternion.identity);
+    }
+}
